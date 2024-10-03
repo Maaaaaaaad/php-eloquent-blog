167ed46cf28d11ecafff47cb677982c722513d06
Maaaaaaaad <wowa-iff@yandex.ru>
2024-10-03 12:40:08 +0300

    work with query builder

--- src/actions/Users.php
+++ src/actions/Users.php
@@ -8,6 +8,7 @@ class Users
 {
     public static function index($params = [])
     {
+        $result = [];
         if (empty($params)) {
             return User::query()->get()->toArray();
         }
@@ -15,21 +16,23 @@ class Users
             if ($kye == 'q') {
                 if (array_key_exists('email', $value)) {
                     $email = $value['email'];
-                } elseif (array_key_exists('first_name', $value)) {
+                    $result[] = User::query()->orWhere('email', $email)->get()->toArray();
+                };
+                if (array_key_exists('first_name', $value)) {
                     $first_name = $value['first_name'];
-                } elseif (array_key_exists('last_name', $value)) {
+                    dump($first_name);
+                    $result[] = User::query()->orWhere('first_name', $first_name)->get()->toArray();
+                    dump($result);
+                };
+                if (array_key_exists('last_name', $value)) {
                     $last_name = $value['last_name'];
-                }
-            } /*elseif ($kye == 's') {
+                    $result[] = User::query()->orWhere('last_name', $last_name)->get()->toArray();
+                };
+            } elseif ($kye == 's') {
                 $sort = explode(':', $value);
-                $user4 = User::query()->orderBy($sort[0], $sort[1])->get()->toArray();
-                print_r($user4);
-            }*/
+                $result = User::query()->orderBy($sort[0], $sort[1])->get();
+            }
         }
-        $result[] = User::query()->where('email', $email)->get()->toArray();
-        $result[] = User::query()->where('first_name', $first_name)->get()->toArray();
-        $result[] = User::query()->where('last_name', $last_name)->get()->toArray();
-
 
         return $result;
     }
