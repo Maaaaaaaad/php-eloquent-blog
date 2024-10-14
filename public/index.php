a5275c70d441beefddbdf14d6785074e5bb7e547
Maaaaaaaad <wowa-iff@yandex.ru>
2024-10-14 21:43:24 +0300

    work with polymorphism

--- public/index.php
+++ public/index.php
@@ -2,35 +2,13 @@
 
 namespace App\KeyValueFunctions;
 
-require __DIR__ . '/../vendor/autoload.php';
+/*require __DIR__ . '/../vendor/autoload.php';*/
 
-use App\Subscription;
-use App\User;
+require_once __DIR__ . '/../src/pages/StaticPage.php';
 
+use App\pages\StaticPage;
 
-
-$user = new User('vasya@email.com', new Subscription('premium'));
-dump($user);
-dump($user->getCurrentSubscription()->hasPremiumAccess()); // true
-dump($user->getCurrentSubscription()->hasProfessionalAccess()); // false
-
-$user = new User('vasya@email.com', new Subscription('professional'));
-dump($user);
-dump($user->getCurrentSubscription()->hasPremiumAccess()); // false
-dump($user->getCurrentSubscription()->hasProfessionalAccess()); // true
-
-// Внутри создается фейковая, потому что подписка не передается
-
-$user = new User('vasya@email.com');
-dump($user);
-
-dump($user->getCurrentSubscription()->hasPremiumAccess()); // false
-dump($user->getCurrentSubscription()->hasProfessionalAccess()); // false
-
-
-
-$user = new User('rakhim@hexlet.io'); // администратор, проверяется по емейлу
-dump($user);
-
-dump($user->getCurrentSubscription()->hasPremiumAccess()); // true
-dump($user->getCurrentSubscription()->hasProfessionalAccess()); // true
+$id = 3;
+$page = new StaticPage($id);
+$page->render();
+echo $page->id($id);
